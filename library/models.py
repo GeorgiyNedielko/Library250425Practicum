ac6d1ebdf30f5b07fd8386256382719f4ab548c0
Georgiy Nedielko <georgiynge2025@gmail.com>
2025-11-05 11:52:52 +0100

    Добавил изменения проекта Django

---
+++ library/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f"{self.last_name[0]} {self.first_name}"
